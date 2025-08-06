8560d1f555b4f70b9f76409f626b2b93f739a08b
justin <justin03we@gmail.com>
2025-08-06 18:46:49 +0200

    add that tasks.json is saved/loaded in .todo folder in home directory

--- todo/json.go
+++ todo/json.go
@@ -4,37 +4,65 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
+const taskDirName string = ".todo"
 const taskFileName string = "tasks.json"
 
-func (tasks TaskList) SaveTasks() error {
-	jsonData, err := json.MarshalIndent(tasks, "", "  ") // MarshalIndent für schön formatierte JSON
+func getTaskFilePath() (string, error) {
+	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		return fmt.Errorf("fehler beim marshalling der aufgaben: %w", err)
+		return "", fmt.Errorf("could not find home directory: %w", err)
 	}
 
-	err = os.WriteFile(taskFileName, jsonData, 0644) // 0644 sind Standard-Dateirechte (lesbar für alle, schreibbar für Besitzer)
+	tasksDir := filepath.Join(homeDir, taskDirName)
+
+	err = os.MkdirAll(tasksDir, 0755)
 	if err != nil {
-		return fmt.Errorf("fehler beim schreiben der aufgaben in die datei: %w", err)
+		return "", fmt.Errorf("could not creat .todo directory: %w", err)
+	}
+
+	return filepath.Join(tasksDir, taskFileName), nil
+}
+
+func (tasks TaskList) SaveTasks() error {
+	filePath, err := getTaskFilePath()
+	if err != nil {
+		return err
+	}
+
+	jsonData, err := json.MarshalIndent(tasks, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error while marshalling tasks: %w", err)
+	}
+
+	err = os.WriteFile(filePath, jsonData, 0644)
+	if err != nil {
+		return fmt.Errorf("error while writing tasks into file: %w", err)
 	}
 
 	return nil
 }
 
 func (tasks *TaskList) LoadTasks() error {
-	jsonData, err := os.ReadFile(taskFileName)
+	filePath, err := getTaskFilePath()
+	if err != nil {
+		return err
+	}
+
+	jsonData, err := os.ReadFile(filePath)
 	if err != nil {
 		if os.IsNotExist(err) {
 			*tasks = TaskList{}
 			return nil
 		}
-		return fmt.Errorf("fehler beim lesen der aufgaben-Datei: %w", err)
+		return fmt.Errorf("error while reading tasks file: %w", err)
 	}
 
 	err = json.Unmarshal(jsonData, tasks)
 	if err != nil {
-		return fmt.Errorf("fehler beim unmarshalling der aufgaben: %w", err)
+		return fmt.Errorf("error while unmarshalling tasks: %w", err)
 	}
 
 	return nil
